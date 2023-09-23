d5cd1c6c6563d35f9f929f1725b6dc25a218ed26
bpOETfJlwB <victormjvizcarra@gmail.com>
2023-09-22 23:22:13 -0700

    Primer parcial

---
+++ main.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const scrollToBottomButtom = document.getElementById('btn-ir-arriba');
+  
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        scrollToBottomButtom.style.display = 'block';
+      } else {
+        scrollToBottomButtom.style.display = 'none';
+      }
+    });
+  
+    scrollToBottomButtom.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  
+  });
+
+
+const contactoForm = document.getElementById('contacto-form');
+
+contactoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const email = document.getElementById("email").value;
+  const mensaje = document.getElementById("mensaje").value;
+
+  alert('Gracias por contactarnos - Nombre: ' + nombre + '- Email: ' + email + '- Mensaje: ' + mensaje);
+
+  contactoForm.reset();
+});
